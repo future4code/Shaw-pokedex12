e9cc17f6eb0d2573068140335d9123aa85fec608
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-05-06 22:06:32 -0300

    Alteração na pokedex inteira

---
+++ pokedex/pokedexx/src/routers/coordenation.js
@@ -0,0 +1,13 @@
+
+export const goToHomePage =(navigate)=>{ 
+    navigate("/");
+}
+export const goToPokedex=(navigate)=>{ 
+    navigate("/pokedex");
+}
+export const goToDetails=(navigate, id)=>{ 
+    navigate(`/${id}`);
+}
+export const goToReturn=(navigate)=>{ 
+    navigate(-1);
+}
